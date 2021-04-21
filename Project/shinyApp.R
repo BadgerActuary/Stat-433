cd8daad6897072161321f0a2a73a19f4bc39e70a
Dingyi <dyli283@gmail.com>
2021-04-20 19:22:51 -0500

    Add 2nd variable with map, add scatter plot

--- Project/shinyApp.R
+++ Project/shinyApp.R
@@ -29,8 +29,7 @@ ui = fluidPage(
            fluidRow(column(4,selectInput("St","State:",c("All",unique(as.character(dt$State))))),
                     column(4,selectInput("Cty","County:",c("All",unique(as.character(dt$County)))))
            ),
-                          
-           
+
            DT::dataTableOutput("myTable")
             
         )
@@ -42,13 +41,21 @@ ui = fluidPage(
         sidebarPanel(
           helpText("Create county maps given a selected variable in a state."),
                           
-          selectInput("var","Variable",
+          selectInput("var1","Variable1",
             choices = c("HPI", "Personal_Income",
                         "Poverty_Percentage", "Population",
                         "HighSchoolLess", "HighSchoolOnly",
                         "SomeCollege", "BachelorAndHigher",
                         "Unemployment_Rate"),
                         selected = "HPI"
+          ),
+          selectInput("var2","Variable2",
+                      choices = c("HPI", "Personal_Income",
+                                  "Poverty_Percentage", "Population",
+                                  "HighSchoolLess", "HighSchoolOnly",
+                                  "SomeCollege", "BachelorAndHigher",
+                                  "Unemployment_Rate"),
+                      selected = "Personal_Income"
           ),
           selectInput("St2",
                     "State:",
@@ -61,10 +68,24 @@ ui = fluidPage(
                       selected = "TRUE"
             
           ),
+          selectInput("size",
+                      "Front Size",
+                      choices = c(1,2,3,4,5,6),
+                      selected = 3
+                      
+          ),
           width=2
         ),
        
-      mainPanel(plotOutput("myMap"))
+      mainPanel(
+        # fluidRow(
+        #   column(4,plotOutput("Map1")),
+        #   column(12,plotOutput("Map2"))
+        # )
+        fluidRow(
+          splitLayout(cellWidths = c("33%", "33%","33%"), plotOutput("Map1"), plotOutput("Map2"), plotOutput("Scatter"))
+        )
+      )
      )
     )
   )
@@ -83,8 +104,8 @@ server = function(input, output) {
     temp[, input$show_vars, drop = FALSE]
   }))
   
-  output$myMap = renderPlot({
-    data = switch(input$var, 
+  output$Map1 = renderPlot({
+    data = switch(input$var1, 
                    "HPI"=dt[,c(1,2,3,12,13)], 
                    "Personal_Income"=dt[,c(1,2,4,12,13)],
                    "Poverty_Percentage"=dt[,c(1,2,5,12,13)], 
@@ -95,7 +116,7 @@ server = function(input, output) {
                    "BachelorAndHigher"=dt[,c(1,2,10,12,13)],
                    "Unemployment_Rate"=dt[,c(1,2,11,12,13)]
     )
-    var = input$var
+    var = input$var1
     
     inputState = subset(us_states, region == tolower(input$St2))
     State_county = subset(us_counties, region == tolower(input$St2))
@@ -119,15 +140,93 @@ server = function(input, output) {
       geom_polygon(fill = "white")+ 
       geom_polygon(data = plot_dt, aes(fill = plot_dt[,var]), color = "grey") +
       scale_fill_distiller(palette = "Blues", trans = "reverse")+
-      geom_text(data=cnames, aes(long, lat, label = subregion, group=subregion), color = "black", size=5,check_overlap = input$overlap)+
+      geom_text(data=cnames, aes(long, lat, label = subregion, group=subregion), color = "black", size=as.numeric(input$size),check_overlap = input$overlap)+
       geom_polygon(color = "black", fill = NA) +
       theme_bw() +
       ditch_the_axes + 
       guides(fill=guide_legend(title=var))
 
   },
-  height = 1000, width = 1500
+  height = 400, width = 550
   )
+  
+  output$Map2 = renderPlot({
+    data = switch(input$var2, 
+                  "HPI"=dt[,c(1,2,3,12,13)], 
+                  "Personal_Income"=dt[,c(1,2,4,12,13)],
+                  "Poverty_Percentage"=dt[,c(1,2,5,12,13)], 
+                  "Population"=dt[,c(1,2,6,12,13)],
+                  "HighSchoolLess"=dt[,c(1,2,7,12,13)], 
+                  "HighSchoolOnly"=dt[,c(1,2,8,12,13)],
+                  "SomeCollege"=dt[,c(1,2,9,12,13)], 
+                  "BachelorAndHigher"=dt[,c(1,2,10,12,13)],
+                  "Unemployment_Rate"=dt[,c(1,2,11,12,13)]
+    )
+    var = input$var2
+    
+    inputState = subset(us_states, region == tolower(input$St2))
+    State_county = subset(us_counties, region == tolower(input$St2))
+    
+    plot_dt = inner_join(State_county, data[which(dt$State_Name == input$St2),], by = c("subregion"="County_Name"))
+    
+    ditch_the_axes = theme(
+      axis.text = element_blank(),
+      axis.line = element_blank(),
+      axis.ticks = element_blank(),
+      panel.border = element_blank(),
+      panel.grid = element_blank(),
+      axis.title = element_blank()
+    )
+    
+    cnames = aggregate(cbind(long,lat)~subregion, data=State_county,
+                       FUN=function(x)mean(range(x)))
+    
+    ggplot(data = inputState, mapping = aes(x = long, y = lat, group = group)) + 
+      coord_fixed(1.3) + 
+      geom_polygon(fill = "white")+ 
+      geom_polygon(data = plot_dt, aes(fill = plot_dt[,var]), color = "grey") +
+      scale_fill_distiller(palette = "Blues", trans = "reverse")+
+      geom_text(data=cnames, aes(long, lat, label = subregion, group=subregion), color = "black", size=as.numeric(input$size),check_overlap = input$overlap)+
+      geom_polygon(color = "black", fill = NA) +
+      theme_bw() +
+      ditch_the_axes + 
+      guides(fill=guide_legend(title=var))
+    
+  },
+  height = 400, width = 550
+  )
+  
+  output$Scatter = renderPlot({
+    Variable1 = switch(input$var1, 
+                  "HPI"=dt[which(dt$State_Name == input$St2),c(3)], 
+                  "Personal_Income"=dt[which(dt$State_Name == input$St2),c(4)],
+                  "Poverty_Percentage"=dt[which(dt$State_Name == input$St2),c(5)], 
+                  "Population"=dt[which(dt$State_Name == input$St2),c(6)],
+                  "HighSchoolLess"=dt[which(dt$State_Name == input$St2),c(7)], 
+                  "HighSchoolOnly"=dt[which(dt$State_Name == input$St2),c(8)],
+                  "SomeCollege"=dt[which(dt$State_Name == input$St2),c(9)], 
+                  "BachelorAndHigher"=dt[which(dt$State_Name == input$St2),c(10)],
+                  "Unemployment_Rate"=dt[which(dt$State_Name == input$St2),c(11)]
+    )
+    Variable2 = switch(input$var2, 
+                  "HPI"=dt[which(dt$State_Name == input$St2),c(3)], 
+                  "Personal_Income"=dt[which(dt$State_Name == input$St2),c(4)],
+                  "Poverty_Percentage"=dt[which(dt$State_Name == input$St2),c(5)], 
+                  "Population"=dt[which(dt$State_Name == input$St2),c(6)],
+                  "HighSchoolLess"=dt[which(dt$State_Name == input$St2),c(7)], 
+                  "HighSchoolOnly"=dt[which(dt$State_Name == input$St2),c(8)],
+                  "SomeCollege"=dt[which(dt$State_Name == input$St),c(9)], 
+                  "BachelorAndHigher"=dt[which(dt$State_Name == input$St2),c(10)],
+                  "Unemployment_Rate"=dt[which(dt$State_Name == input$St2),c(11)]
+    )
+    temp2 = cbind(Variable1, Variable2)
+    colnames(temp2)=c(input$var1,input$var2)
+    ggplot(data=as.data.frame(temp2), aes(x=temp2[,1],y=temp2[,2]))+
+      geom_point(color="blue")+
+      geom_smooth(method="lm", se=FALSE, color="red")+
+      labs(x=input$var1, y=input$var2)
+  })
+
 }
 
 # Run app ----
